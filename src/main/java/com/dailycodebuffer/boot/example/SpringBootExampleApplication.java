084071c3b79af1773c513328c5248fd356e4b58a
Vishrut Majmudar <v.h.majmudar@gmail.com>
2023-11-05 11:49:50 +0530

    Setting up New Spring Boot Project with Spring Initializr

---
+++ src/main/java/com/dailycodebuffer/boot/example/SpringBootExampleApplication.java
@@ -0,0 +1,13 @@
+package com.dailycodebuffer.boot.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootExampleApplication.class, args);
+	}
+
+}
