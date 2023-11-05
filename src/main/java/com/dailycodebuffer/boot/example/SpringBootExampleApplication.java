11655ab189425ec2776e40ae827167b6d9fabd48
Vishrut Majmudar <v.h.majmudar@gmail.com>
2023-11-05 23:43:45 +0530

    Spring Boot Actuator

--- src/main/java/com/dailycodebuffer/boot/example/SpringBootExampleApplication.java
+++ src/main/java/com/dailycodebuffer/boot/example/SpringBootExampleApplication.java
@@ -1,8 +1,11 @@
 package com.dailycodebuffer.boot.example;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.health.Health;
+import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
@@ -26,6 +29,5 @@ public class SpringBootExampleApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootExampleApplication.class, args);
 	}
-
 
 }
