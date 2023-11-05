fa658a8ff8839878f72a714da5c69faab5432b50
Vishrut Majmudar <v.h.majmudar@gmail.com>
2023-11-05 19:09:16 +0530

    Embedded Server

--- src/main/java/com/dailycodebuffer/boot/example/component/HelloWorldComponent.java
+++ src/main/java/com/dailycodebuffer/boot/example/component/HelloWorldComponent.java
@@ -1,10 +1,18 @@
 package com.dailycodebuffer.boot.example.component;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
+
 
 @Component
 public class HelloWorldComponent {
+    Logger logger = LoggerFactory.getLogger(HelloWorldComponent.class);
+
     public String sayHello() {
+        logger.debug("Debugging from Application component.");
+        logger.trace("Tracing from Application component.");
+        logger.info("info from Application component.");
         return "Hello World of Spring Boot!!";
     }
 }
